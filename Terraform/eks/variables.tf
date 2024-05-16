259fe97e7a94fd2d1379f1311d7c7c8deaa867d8
Proud of JW <154409699+oncliff-climing@users.noreply.github.com>
2024-05-16 14:58:33 +0900

    Update variables.tf

--- Terraform/eks/variables.tf
+++ Terraform/eks/variables.tf
@@ -1,26 +1,31 @@
 variable "region" {
-  description = "ap-northeast-1"  # 배포할 AWS 리전
+  description = "The AWS region to deploy to"  # 배포할 AWS 리전
   type        = string
+  default     = "ap-northeast-1"
 }
 
 variable "cluster_name" {
-  description = "EOF_EKS"  # EKS 클러스터 이름
+  description = "The name of the EKS cluster"  # EKS 클러스터 이름
   type        = string
+  default     = "EOF_EKS"
 }
 
 variable "cluster_role_arn" {
-  description = "arn:aws:iam::058264352854:role/eksClusterRole"  # EKS 클러스터에 할당할 IAM 역할의 ARN
+  description = "The ARN of the IAM role to use for the EKS cluster"  # EKS 클러스터에 할당할 IAM 역할의 ARN
   type        = string
+  default     = "arn:aws:iam::058264352854:role/eksClusterRole"
 }
 
 variable "node_group_name" {
-  description = "EOF"  # 노드 그룹 이름
+  description = "The name of the node group"  # 노드 그룹 이름
   type        = string
+  default     = "EOF"
 }
 
 variable "node_role_arn" {
-  description = "arn:aws:iam::058264352854:instance-profile/EC2"  # 노드 그룹에 할당할 IAM 역할의 ARN
+  description = "The ARN of the IAM role to use for the node group"  # 노드 그룹에 할당할 IAM 역할의 ARN
   type        = string
+  default     = "arn:aws:iam::058264352854:instance-profile/EC2"
 }
 
 variable "subnet_ids" {
@@ -31,21 +36,25 @@ variable "subnet_ids" {
 variable "instance_types" {
   description = "A list of instance types for the node group"  # 노드 그룹에 사용할 인스턴스 유형 목록
   type        = list(string)
+  default     = ["t3.medium"]
 }
 
 variable "desired_capacity" {
   description = "The desired number of nodes in the node group"  # 원하는 노드 수
-  type        = 1
+  type        = number
+  default     = 1
 }
 
 variable "max_capacity" {
   description = "The maximum number of nodes in the node group"  # 최대 노드 수
-  type        = 3
+  type        = number
+  default     = 3
 }
 
 variable "min_capacity" {
   description = "The minimum number of nodes in the node group"  # 최소 노드 수
-  type        = 1
+  type        = number
+  default     = 1
 }
 
 variable "tags" {
