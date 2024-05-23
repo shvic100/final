6cb4428c9ba29a63e03b3777b302ae6a2720a31c
Proud of JW <154409699+oncliff-climing@users.noreply.github.com>
2024-05-23 17:28:47 +0900

    Update runner.py

--- back/runner.py
+++ back/runner.py
@@ -1,9 +1,10 @@
-import mysql.connector                      # MySQL 데이터베이스와 상호 작용하기 위해 
-import argparse                             # 명령줄 인수를 처리
-import logging                              # 로그 기록
-import requests                             # HTTP 요청 보내기 위해
-import gevent                               # 비동기 작업을 위해 greenlet 사용
-from stats_management import RequestStats   # 요청 통게 관리
+import mysql.connector
+import argparse
+import logging
+import requests
+import gevent
+from stats_management import RequestStats
+from datetime import datetime, timedelta
 
 # MySQL 데이터베이스 연결
 db_config = {
@@ -13,62 +14,78 @@ db_config = {
     'database': 'test'    
 }
 
-conn = mysql.connector.connect(**db_config)     # DB 연결 객체
-c = conn.cursor()                               # DB 커서 객체
+conn = mysql.connector.connect(**db_config)
+c = conn.cursor()
 
 # result 테이블 생성
-c.execute('''CREATE TABLE IF NOT EXISTS result (
-             test_id INT,
-             average_response_time FLOAT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS spike (
+    test_id INT PRIMARY KEY,
+    Failures INT,
+    avg_response_time DECIMAL(10, 2),
+    num_user INT,
+    load_duration TIME)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS incremental (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    test_id INT,
+    RPS DECIMAL(10, 2),
+    Failures_per_second DECIMAL(10, 2),
+    avg_response_time DECIMAL(10, 2),
+    number_of_users INT,
+    recorded_time TIME)''')
 
 # 가상의 사용자 클래스 정의 : 각 사용자가 요청을 보내는 역할
 class User:
-
     # 클래스 정의 및 초기화
     def __init__(self, environment, url):
-        self.environment = environment # 테스트 환경 객체 저장
-        self.url = url  # 요청을 보낼 url
+        self.environment = environment
+        self.url = url  
 
     # 지정된 url로 HTTP GET 요청 보내고 응답 시간 기록
     def do_work(self):
         try:
-            response = requests.get(self.url)                       # 지정된 URL로 GET 요청 보냄
-            response.raise_for_status()                             # HTTP 상태 코드가 200(성공)이 아닌 경우 예외를 발생
-            response_time = response.elapsed.total_seconds() * 1000 # 응답 시간을 밀리초 단위로 변환
-            content_length = len(response.content)                  # 응답 내용의 길이를 바이트 단위로 계산
+            response = requests.get(self.url)
+            response.raise_for_status()
+            response_time = response.elapsed.total_seconds() * 1000
+            content_length = len(response.content)
             
             if self.environment.stats:
-                self.environment.stats.log_request('GET', self.url, response_time, content_length) # 요청 통계 기록
-            self.environment.load_tester.response_times.append(response_time)  # 응답 시간을 LoadTester 객체의 response_times 리스트에 추가
-            # 요청 성공과 관련된 디버그 정보를 로그로 기록
+                self.environment.stats.log_request('GET', self.url, response_time, content_length)
+            self.environment.load_tester.response_times.append(response_time)  
             logging.debug(f"Request to {self.url} successful. Response time: {response_time} ms, Content length: {content_length} bytes")
         
         except requests.RequestException as e:
-            logging.error(f"An error occurred while requesting {self.url}: {e}") # 예외를 처리하고, 오류 로그를 기록
+            self.environment.load_tester.failures += 1 
+            logging.error(f"An error occurred while requesting {self.url}: {e}")
 
-# 부하 테스트 클래스 정의 : 부하 테스트를 수행하며, 여러 사용자를 동시에 생성하고 요청을 관리
+# 부하 테스트 클래스 정의
 class LoadTester:
     def __init__(self, environment):
         self.environment = environment
-        self.response_times = []  # 응답 시간을 저장할 리스트
+        self.response_times = []  
+        self.failures = 0 
+        self.request_count = 0  # 초당 요청 수를 추적하기 위한 요청 수 초기화
+        self.start_time = datetime.now()  # 테스트 시작 시간 기록
 
     # 지정된 수의 사용자 객체 생성 -> 동시 요청 처리
     def spawn_users(self, user_count, url):
-        users = [] # 생성된 User 객체를 저장할 리스트
-        greenlets = [] # greenlet 객체를 저장할 리스트
+        users = [] 
+        greenlets = [] 
         for _ in range(user_count):
             user = User(self.environment, url)
             users.append(user)
-            greenlets.append(gevent.spawn(user.do_work)) # User 객체의 do_work 메서드 비동기적으로 실행
-        gevent.joinall(greenlets) # 모든 greenlet이 완료될 때까지 기다림.
-        return users # 사용자를 생성하고 요청을 수행한 후 users 리스트를 반환
+            greenlets.append(gevent.spawn(user.do_work)) 
+        gevent.joinall(greenlets) 
+        self.request_count += user_count  # 새로 추가된 사용자 수만큼 요청 수 증가
+        return users 
 
     # 주기적으로 사용자를 추가하여 부하 테스트를 실행
-    # 초기 사용자 수만큼 사용자를 생성하고, 지정된 간격마다 추가 사용자를 생성하여 부하 테스트를 실행
-    def add_users_periodically(self, initial_users, additional_users, interval, repeat_count, url):
+    def add_users_periodically(self, initial_users, additional_users, interval, repeat_count, url, test_id):
         self.spawn_users(initial_users, url)
-        for _ in range(repeat_count):
-            gevent.sleep(interval)
+        for i in range(repeat_count + 1):
+            for _ in range(interval):  # 1초마다 record_incremental_stats 호출
+                gevent.sleep(1)  # 1초 대기
+                self.record_incremental_stats(test_id)
             self.spawn_users(additional_users, url)
 
     # 응답 시간 리스트의 평균을 계산하여 반환
@@ -78,31 +95,75 @@ class LoadTester:
         else:
             return 0
 
+    # 실패율을 계산하여 반환
+    def calculate_failure_rate(self):
+        total_requests = len(self.response_times) + self.failures
+        if total_requests > 0:
+            return (self.failures / total_requests) * 100
+        else:
+            return 0
+    
+    # 초당 통계를 기록
+    def record_incremental_stats(self, test_id):
+        current_time = datetime.now().time()
+        average_response_time = self.calculate_average_response_time()
+        failures_per_second = self.failures / len(self.response_times) if self.response_times else 0
+        elapsed_time = (datetime.now() - self.start_time).total_seconds()
+        rps = self.request_count / elapsed_time if elapsed_time > 0 else 0
+
+        c.execute('''INSERT INTO incremental (test_id, RPS, Failures_per_second, avg_response_time, number_of_users, recorded_time)
+                     VALUES (%s, %s, %s, %s, %s, %s)''',
+                  (test_id, rps, failures_per_second, average_response_time, len(self.response_times), current_time))
+        conn.commit()
+
+    # 최종 통계를 기록 (스파이크 테스트 전용)
+    def record_final_stats_spike(self, test_id, load_duration):
+        average_response_time = self.calculate_average_response_time()
+        failure_rate = self.calculate_failure_rate()
+        num_users = len(self.response_times)
+        
+        c.execute('''INSERT INTO spike (test_id, Failures, avg_response_time, num_user, load_duration)
+                     VALUES (%s, %s, %s, %s, %s)''',
+                  (test_id, self.failures, average_response_time, num_users, str(load_duration)))
+        conn.commit()
+
 # 테스트 환경 설정 클래스 정의
 class TestEnvironment:
     def __init__(self):
-        self.stats = RequestStats()  # RequestStats 인스턴스로 초기화
-        self.load_tester = LoadTester(self) # LoadTester 객체 생성
+        self.stats = RequestStats()
+        self.load_tester = LoadTester(self)
 
 # 테스트 환경 설정
-def setup_test(): # 로깅 설정 후 TestEnvironment 객체 초기화
-    logging.basicConfig(level=logging.DEBUG) # 디버그 수준으로 로깅을 설정
-    environment = TestEnvironment() # 테스트 환경 객체를 초기화
-    return environment.load_tester # 초기화된 LoadTester 객체를 반환
+def setup_test():
+    logging.basicConfig(level=logging.DEBUG)
+    environment = TestEnvironment()
+    return environment.load_tester
 
 # 부하 테스트를 설정하고 실행
 def main(url, initial_user_count, additional_user_count, interval, repeat_count, test_id):
     load_tester = setup_test()
-    # 부하 테스트 실행
-    load_tester.add_users_periodically(initial_user_count, additional_user_count, interval, repeat_count, url)
-    # 평균 응답 시간 계산
+    
+    start_time = datetime.now() 
+    
+    if additional_user_count == 0 or interval == 0 or repeat_count == 0:
+        # 스파이크 테스트: 초기 사용자 수만큼 요청을 보냄
+        print("Performing spike test...")
+        load_tester.spawn_users(initial_user_count, url)
+        end_time = datetime.now() 
+        load_duration = end_time - start_time 
+        load_tester.record_final_stats_spike(test_id, load_duration)
+    else:
+        # 점진적 테스트: 주기적으로 사용자를 추가하며 요청을 보냄
+        print("Performing incremental test...")
+        load_tester.add_users_periodically(initial_user_count, additional_user_count, interval, repeat_count, url, test_id)
+    
     average_response_time = load_tester.calculate_average_response_time()
-    # 결과 데이터베이스에 저장
-    c.execute("INSERT INTO result (test_id, average_response_time) VALUES (%s, %s)",
-              (test_id, average_response_time))
-    # 데이터베이스에 변경 사항을 커밋
-    conn.commit()
-    print(f"######## Average Response Time: {average_response_time} ms ######## ")
+    failure_rate = load_tester.calculate_failure_rate()
+    load_duration = datetime.now() - start_time
+    
+    print(f"######## Average Response Time: {average_response_time} ms ########")
+    print(f"######## Failure Rate: {failure_rate}% ########")
+    print(f"######## Load Duration: {load_duration} ########")
 
 # 명령줄 인수 처리
 if __name__ == "__main__":
