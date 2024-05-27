a5ff2fc365120f16195c6eb614eff62bc56dc2e0
Proud of JW <154409699+oncliff-climing@users.noreply.github.com>
2024-05-27 21:18:12 +0900

    Update variables.tf

--- Terraform/modules_test/iam/variables.tf
+++ Terraform/modules_test/iam/variables.tf
@@ -1,5 +1,5 @@
 variable "tags" {
-  description = "A map of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
+  description = "A map of tags to assign to the resources."  # 리소스에 할당할 태그의 맵
+  type        = map(string)  # 문자열 맵 타입으로 지정
+  default     = {}  # 기본값 설정 (빈 맵)
 }
