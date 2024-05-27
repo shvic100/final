58e80b5e644988d39ec3dbb080ac82d3269a2340
Proud of JW <154409699+oncliff-climing@users.noreply.github.com>
2024-05-27 21:18:35 +0900

    Update outputs.tf

--- Terraform/modules_test/vpc/outputs.tf
+++ Terraform/modules_test/vpc/outputs.tf
@@ -1,11 +1,14 @@
+# VPC ID 출력
 output "vpc_id" {
-  value = aws_vpc.EOF_vpc.id
+  value = aws_vpc.EOF_vpc.id  # 생성된 VPC의 ID를 출력
 }
 
+# 퍼블릭 서브넷 ID 목록 출력
 output "public_subnet_ids" {
-  value = [aws_subnet.public_a.id, aws_subnet.public_c.id]
+  value = [aws_subnet.public_a.id, aws_subnet.public_c.id]  # 생성된 퍼블릭 서브넷의 ID 목록을 출력
 }
 
+# 프라이빗 서브넷 ID 목록 출력
 output "private_subnet_ids" {
-  value = [aws_subnet.private_a.id, aws_subnet.private_c.id]
+  value = [aws_subnet.private_a.id, aws_subnet.private_c.id]  # 생성된 프라이빗 서브넷의 ID 목록을 출력
 }
