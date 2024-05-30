e2bc0a9d29e0e7ca0dfa3dca4a64cb3a8d00805f
Proud of JW <154409699+oncliff-climbing@users.noreply.github.com>
2024-05-30 18:58:06 +0900

    Update main.py

--- back-app/main.py
+++ back-app/main.py
@@ -16,12 +16,6 @@ def health_check():
 def read_root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
